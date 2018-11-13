e825fcf6f4795fbdc1d721e2a4323427d00670da
Razvan Uscatu <razvanusc@gmail.com>
2018-11-13 12:24:48 +0100

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all..."
+User.destroy_all
+Lesson.destroy_all
+
+puts "Seeding..."
+
+User.create([
+  {
+    email: "frank@gmail.com",
+    password: "123secret",
+    first_name: "Frank",
+    last_name: "Alkema",
+    phone_number: "1234567890",
+    bio: "I am Frank",
+    website_url: "iamfrank.com",
+    facebook_url: "facebook.com",
+    instagram_url: "instagram.com",
+    available: true,
+    address: "Amsterdam"
+  },
+  {
+    email: "jordy@gmail.com",
+    password: "123secret",
+    first_name: "Jordy",
+    last_name: "Kleberg",
+    phone_number: "1234567890",
+    bio: "I am Jordy",
+    website_url: "iamjordy.com",
+    facebook_url: "facebook.com",
+    instagram_url: "instagram.com",
+    available: true,
+    address: "Haarlem"
+  },
+  {
+    email: "max@gmail.com",
+    password: "123secret",
+    first_name: "Max",
+    last_name: "Fontana",
+    phone_number: "1234567890",
+    bio: "I am Max",
+    website_url: "iammax.com",
+    facebook_url: "facebook.com",
+    instagram_url: "instagram.com",
+    available: true,
+    address: "Utrecht"
+  }
+])
+
+
+Lesson.create([
+  {
+    :start_time => DateTime.strptime("17:00", "%H:%M"),
+    :start_date => DateTime.strptime("09/01/2019", "%m/%d/%Y"),
+    duration: 50,
+    price: 20,
+    address: "Amsterdam",
+    description: "Yoga class"
+  },
+  {
+    :start_time => DateTime.strptime("8:00", "%H:%M"),
+    :start_date => DateTime.strptime("09/14/2019", "%m/%d/%Y"),
+    duration: 40,
+    price: 10,
+    address: "Haarlem",
+    description: "Yoga class"
+  },
+  {
+    :start_time => DateTime.strptime("9:00", "%H:%M"),
+    :start_date => DateTime.strptime("09/14/2019", "%m/%d/%Y"),
+    duration: 50,
+    price: 20,
+    address: "Utrecht",
+    description: "Yoga class"
+  },
+  {
+    :start_time => DateTime.strptime("10:00", "%H:%M"),
+    :start_date => DateTime.strptime("09/14/2019", "%m/%d/%Y"),
+    duration: 60,
+    price: 50,
+    address: "Maastricht",
+    description: "Yoga class"
+  }
+])
