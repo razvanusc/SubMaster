41a6b014b44b6dca6c0f0213e58925fadbabcd4f
Krisztina Kovari <krystonen@gmail.com>
2018-11-20 23:02:10 +0100

    Paved the path through bookings and back to classes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
   resources :lessons do
-    resources :bookings, only: [:create, :new]
+    resources :bookings, only: [:index, :create, :new]
   end
   resources :bookings, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
